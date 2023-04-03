5bd3c8dced6a0c964070d0cf688ed5bfe5edb807
Philip Rydqvist <90709110+PhilipRydqvist@users.noreply.github.com>
2023-04-03 14:05:47 +0200

    faq site and index

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -2,10 +2,10 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
-import Link from 'next/link';
+
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function Faq() {
   return (
     <>
       <Head>
@@ -16,9 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p> COOOKIES!!</p>
-          <p> Läs Readme.md</p>
-          <Link href="/faq">Faq</Link>
+          <p> Information om Cookies</p>
         </div>
       </main>
     </>
